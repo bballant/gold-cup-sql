3a2164f67617515c8f85f68d54530ee2b956f191
Brian Ballantine <brianballantine@gmail.com>
2025-06-19 22:46:15 -0400

    mocked header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,32 +13,31 @@
   };
 </script>
 
-<h1 class="text-4xl font-bold mb-6 text-center">
-  <span class="text-warning-9">Gold</span>
+<h1 class="text-4xl preset-filled-surface-800-200 max-w-6xl mx-auto text-warning-contrast-900-100 font-bold mb-6 pt-4 pb-4 text-center">
   <img src="/goldcup.png" alt="Gold Cup" class="h-8 inline mx-2" />
-  <span class="text-warning-9">Cup</span>
-  SQL
+  <span class="text-warning-9">Gold Cup SQL</span>
+  <img src="/goldcup.png" alt="Gold Cup" class="h-8 inline mx-2" />
 </h1>
 
-<h2 class="text-2xl font-semibold mb-2 text-center">Connection Details</h2>
-<div class="card  preset-filled-surface-200-800 max-w-xs mx-auto mb-6">
-  <div class="card-body text-center">
+<h2 class="text-2xl font-semibold mb-2 mt-2 text-center">Connection Details</h2>
+<div class="border-2 border-surface-950-50 max-w-xs mx-auto mb-6 pt-4">
+  <div>
     <table class="table-auto mx-auto mb-4">
       <tbody>
         <tr>
-          <td class="px-4 py-2 font-semibold">Database</td>
+          <td class="px-4 py-2 font-semibold text-right">Database</td>
           <td class="px-4 py-2">{data.connection.database}</td>
         </tr>
         <tr>
-          <td class="px-4 py-2 font-semibold">Host</td>
+          <td class="px-4 py-2 font-semibold text-right">Host</td>
           <td class="px-4 py-2">{data.connection.host}</td>
         </tr>
         <tr>
-          <td class="px-4 py-2 font-semibold">User</td>
+          <td class="px-4 py-2 font-semibold text-right">User</td>
           <td class="px-4 py-2">{data.connection.user}</td>
         </tr>
         <tr>
-          <td class="px-4 py-2 font-semibold">Port</td>
+          <td class="px-4 py-2 font-semibold text-right">Port</td>
           <td class="px-4 py-2">{data.connection.port}</td>
         </tr>
       </tbody>
