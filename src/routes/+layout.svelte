9dbbe2d1cff9aa12415a93757b4ed8a1dd63e331
Brian Ballantine <brianballantine@gmail.com>
2025-06-19 22:53:14 -0400

    human tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	let { children } = $props();
 </script>
 
-<header class="text-4xl max-w-6xl mx-auto text-center my-6">
+<header class="text-4xl max-w-6xl preset-filled-surface-800-200 mx-auto pb-2 pt-2 mt-2 text-center">
   <img src="/goldcup.png" alt="Gold Cup" class="h-8 inline mx-2" />
   <span class="text-warning-9">Gold Cup SQL</span>
   <img src="/goldcup.png" alt="Gold Cup" class="h-8 inline mx-2" />
